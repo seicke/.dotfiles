85e0067ef47886d927005bcd0cdaf927270d610a
seicke <sebastian.eicke@gmail.com>
2025-11-02 15:03:27 +0100

    Update dock.sh to use correct paths for system applications and ensure Dock restart logic is intact

--- scripts/install/macOs/dock.sh
+++ scripts/install/macOs/dock.sh
@@ -1,18 +1,16 @@
 #!/bin/zsh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
-dockutil --no-restart --add "/Applications/Music.app"
-dockutil --no-restart --add "/Applications/Messages.app"
-dockutil --no-restart --add "/Applications/Notes.app"
+dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
+dockutil --no-restart --add "/System/Applications/Music.app"
+dockutil --no-restart --add "/System/Applications/Messages.app"
+dockutil --no-restart --add "/System/Applications/Notes.app"
 dockutil --no-restart --add "/Applications/Obsidian.app"
-dockutil --no-restart --add "/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/Reminders.app"
-dockutil --no-restart --add "/Applications/Mail.app"
-dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/System/Applications/Calendar.app"
+dockutil --no-restart --add "/System/Applications/Reminders.app"
+dockutil --no-restart --add "/System/Applications/Mail.app"
+dockutil --no-restart --add "/System/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
-
-killall Dock
 
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
@@ -28,3 +26,8 @@ defaults write com.apple.dock no-bouncing -bool true
 
 # Don't show recently used applications in the Dock
 defaults write com.Apple.Dock show-recents -bool false
+
+killall Dock
+
+while ! pgrep -x "Dock" >/dev/null; do sleep 0.1; done
+echo "✅ Dock restarted and settings applied."

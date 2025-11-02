470d1645069ebb99674a463dda956adb1f57f7b5
seicke <sebastian.eicke@gmail.com>
2025-11-02 16:24:45 +0100

    Fix symlinking script to create .config directory if it doesn't exist

--- scripts/update/symlinking.sh
+++ scripts/update/symlinking.sh
@@ -4,4 +4,8 @@ ln -sf $HOME'/.dotfiles/.gitignore' $HOME'/.gitignore'
 ln -sf $HOME'/.dotfiles/.config/.gitconfig' $HOME'/.gitconfig'
 ln -sf $HOME'/.dotfiles/system/.zshrc' $HOME'/.zshrc'
 ln -sf $HOME'/.dotfiles/system/.zprofile' $HOME'/.zprofile'
+
+if [ ! -d "$HOME/.config" ]; then
+    mkdir -p "$HOME/.config"
+fi
 ln -sf $HOME'/.dotfiles/.config/topgrade.toml' $HOME'/.config/topgrade.toml'

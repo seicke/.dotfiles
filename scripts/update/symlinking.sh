69fe244ec391d6abc929376c030d79b48a1cb184
seicke <sebastian.eicke@gmail.com>
2025-11-02 16:17:59 +0100

    Add Topgrade configuration and update symlinking script

--- scripts/update/symlinking.sh
+++ scripts/update/symlinking.sh
@@ -3,3 +3,4 @@
 ln -sf $HOME'/.dotfiles/.gitignore' $HOME'/.gitignore'
 ln -sf $HOME'/.dotfiles/.config/.gitconfig' $HOME'/.gitconfig'
 ln -sf $HOME'/.dotfiles/system/.zshrc' $HOME'/.zshrc'
+ln -sf $HOME'/.dotfiles/.config/topgrade.toml' $HOME'/.config/topgrade.toml'

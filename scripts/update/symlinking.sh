8de8a29e55e066826aeb9554ccd55b0b61730bed
seicke <sebastian.eicke@gmail.com>
2025-11-02 16:26:20 +0100

    Update symlinking script to use zsh and improve directory check syntax

--- scripts/update/symlinking.sh
+++ scripts/update/symlinking.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/zsh
 
 ln -sf $HOME'/.dotfiles/.gitignore' $HOME'/.gitignore'
 ln -sf $HOME'/.dotfiles/.config/.gitconfig' $HOME'/.gitconfig'
 ln -sf $HOME'/.dotfiles/system/.zshrc' $HOME'/.zshrc'
 ln -sf $HOME'/.dotfiles/system/.zprofile' $HOME'/.zprofile'
 
-if [ ! -d "$HOME/.config" ]; then
+if [[ ! -d "$HOME/.config" ]]; then
     mkdir -p "$HOME/.config"
 fi
 ln -sf $HOME'/.dotfiles/.config/topgrade.toml' $HOME'/.config/topgrade.toml'

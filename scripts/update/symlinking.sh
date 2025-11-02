f70315ed2cb54266fe3c54cb2bbdb40881ca9af2
seicke <sebastian.eicke@gmail.com>
2025-11-02 16:21:32 +0100

    Add .zprofile for Homebrew environment setup and update symlinking script

--- scripts/update/symlinking.sh
+++ scripts/update/symlinking.sh
@@ -3,4 +3,5 @@
 ln -sf $HOME'/.dotfiles/.gitignore' $HOME'/.gitignore'
 ln -sf $HOME'/.dotfiles/.config/.gitconfig' $HOME'/.gitconfig'
 ln -sf $HOME'/.dotfiles/system/.zshrc' $HOME'/.zshrc'
+ln -sf $HOME'/.dotfiles/system/.zprofile' $HOME'/.zprofile'
 ln -sf $HOME'/.dotfiles/.config/topgrade.toml' $HOME'/.config/topgrade.toml'
